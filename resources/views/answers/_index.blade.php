96947d5590124162021e2a22008d8ee83b3e217d
BeeSupper <lebinh@BeeSupper-2.local>
2019-07-20 06:48:42 +0700

    Fix error can on view _index.blade.php

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -44,18 +44,18 @@
                         {!! $answer->body_html !!}
                         <div class="row">
                             <div class="col-4">
-                                @if(Auth::user()->can('update', $answer))
-                                <a href="{{ route('questions.answers.edit', [$question->id, $answer->id]) }}"
-                                    class="btn btn-sm btn-outline-info">Edit</a>
+                                @if(Auth::user() && Auth::user()->can('update', $answer))
+                                    <a href="{{ route('questions.answers.edit', [$question->id, $answer->id]) }}"
+                                        class="btn btn-sm btn-outline-info">Edit</a>
                                 @endif
-                                @if(Auth::user()->can('delete', $answer))
-                                <form class="form-delete" method="POST"
-                                    action="{{ route('questions.answers.destroy', [$question->id, $answer->id]) }}">
-                                    @method('DELETE')
-                                    @csrf
-                                    <button class="btn btn-sm btn-outline-danger" type="submit"
-                                        onclick="return confirm('Are u sure?')">Delete</button>
-                                </form>
+                                @if(Auth::user() && Auth::user()->can('delete', $answer))
+                                    <form class="form-delete" method="POST"
+                                        action="{{ route('questions.answers.destroy', [$question->id, $answer->id]) }}">
+                                        @method('DELETE')
+                                        @csrf
+                                        <button class="btn btn-sm btn-outline-danger" type="submit"
+                                            onclick="return confirm('Are u sure?')">Delete</button>
+                                    </form>
                                 @endif
                             </div>
                             <div class="col-4"></div>
