246e11e7b9320e9fe75b5e24da432ba92050a8c0
BeeSupper <lebinh@BeeSupper-2.local>
2019-07-25 18:18:04 +0700

    Beauty the flash message and confirm dialog

--- resources/js/components/Answer.vue
+++ resources/js/components/Answer.vue
@@ -1,61 +1,89 @@
 <script>
 export default {
-    props: ['answer'],
+  props: ["answer"],
 
-    data() {
-        return {
-            editing: false,
-            body: this.answer.body,
-            bodyHtml: this.answer.body_html, 
-            id: this.answer.id,
-            questionId: this.answer.question_id,
-            beforeEditCache: null,
-        }
+  data() {
+    return {
+      editing: false,
+      body: this.answer.body,
+      bodyHtml: this.answer.body_html,
+      id: this.answer.id,
+      questionId: this.answer.question_id,
+      beforeEditCache: null
+    };
+  },
+
+  methods: {
+    edit() {
+      this.beforeEditCache = this.body;
+      this.editing = true;
     },
-
-    methods: {
-        edit(){
-            this.beforeEditCache = this.body; 
-            this.editing = true;
-        },
-        cancel(){
-            this.body = this.beforeEditCache;
-            this.editing = false;
-        },
-        update(){
-            axios.patch(this.endpoint, {
-                body: this.body
-            })
-            .then(res => {
-                // console.log(res);
-                this.editing = false;
-                this.bodyHtml = res.data.body_html;
-                alert(res.data.message);
-            })
-            .catch(err => {
-                alert(err.response.data.message);
-            });
-        },
-        destroy(){
-            if(confirm('Are u sure?')){
-                axios.delete(this.endpoint)
-                .then(res => {
-                    $(this.$el).fadeOut(500, () => {
-                        alert(res.data.message); 
-                    })
-                });
-            }
-        }
+    cancel() {
+      this.body = this.beforeEditCache;
+      this.editing = false;
+    },
+    update() {
+      axios
+        .patch(this.endpoint, {
+          body: this.body
+        })
+        .then(res => {
+          // console.log(res);
+          this.editing = false;
+          this.bodyHtml = res.data.body_html;
+          this.$toast.success(res.data.message, "Success", { timeout: 3000 });
+        })
+        .catch(err => {
+          this.$toast(err.response.data.message, "Error", { timeout: 3000 });
+        });
+    },
+    destroy() {
+      //thong bao confirm cua toast
+      this.$toast.question('Are u sure?', 'Confirm', {
+        timeout: 20000,
+        close: false,
+        overlay: true,
+        displayMode: "once",
+        id: "question",
+        zindex: 999,
+        title: "Hey",
         
-    },
+        position: "center",
+        buttons: [
+          [
+            "<button><b>YES</b></button>",
+            (instance, toast) => {
+                axios.delete(this.endpoint).then(res => {
+                    $(this.$el).fadeOut(500, () => {
+                        // alert(res.data.message);
+                        this.$toast.success(res.data.message, "Success", { timeout: 3000 });
+                    });
+                });
 
-    computed: {
-        isInvalid(){
-            return this.body.length < 10;
-        },
-        endpoint(){
-            return `/questions/${this.questionId}/answers/${this.id}`;
-        }
+              instance.hide({ transitionOut: "fadeOut" }, toast, "button");
+            },
+            true
+          ],
+          [
+            "<button>NO</button>",
+            function(instance, toast) {
+              instance.hide({ transitionOut: "fadeOut" }, toast, "button");
+            }
+          ]
+        ],
+      });
+
+
     }
-}
+  },
+
+  computed: {
+    isInvalid() {
+      return this.body.length < 10;
+    },
+    endpoint() {
+      return `/questions/${this.questionId}/answers/${this.id}`;
+    }
+  }
+};
 </script>
